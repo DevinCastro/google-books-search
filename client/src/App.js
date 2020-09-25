7e207a95cd2fdafd410b74e35df497355a99d683
Devin Castro <dcrocker21@gmail.com>
2020-09-24 22:57:48 -0700

    brought in react strap and set up router

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Home from './pages/Home'
+import Saved from './pages/Saved'
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link
+} from 'react-router-dom'
+
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  Nav,
+  NavItem,
+  NavLink
+} from 'reactstrap'
+
 
 const App = () => {
+
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
+
   return (
-    <h1>Hello World!</h1>
+    <Router>
+      <div>
+        <Navbar color="dark" light expand="md">
+          <NavbarToggler onClick={toggle} />
+          <Collapse isOpen={isOpen} navbar>
+            <Nav className="mr-auto" navbar>
+              <NavItem>
+                <NavLink><Link to="/">Home</Link></NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink><Link to="/saved">Saved</Link></NavLink>
+              </NavItem>
+
+            </Nav>
+          </Collapse>
+        </Navbar>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/saved" component={Saved} />
+        </Switch>
+      </div>
+    </Router>
   )
 }
 
---
+++ client/src/pages/Saved/Saved.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Saved = props => {
+  return (
+    <h1>Saved page</h1>
+  )
+}
+
+export default Saved
