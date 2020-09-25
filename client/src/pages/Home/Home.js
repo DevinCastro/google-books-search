fbc404a1c39178adcb58f8e8b52c0b06be57a784
Devin Castro <dcrocker21@gmail.com>
2020-09-24 23:19:44 -0700

    form on search page

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,8 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Container, Jumbotron, Button } from 'reactstrap';
+import { Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+// import axios from 'axios'
+
 
 const Home = props => {
+
+  const [bookState, setBookState] = useState({
+    book: '',
+    books: []
+  })
+
+  bookState.handleInputChange = event => {
+    setBookState({ ...bookState, [event.target.name]: event.target.value })
+  }
+
+  bookState.handleBookSearch = event => {
+    event.preventDefault()
+    console.log('vince vapes')
+  }
+
+
+
   return (
-    <h1>Hello</h1>
+    <>
+      <Container>
+        <div>
+
+          <Jumbotron>
+            <h1 className="display-3">Google Book Search</h1>
+            <p className="lead">Search or your favorite books! Then save them to your dashboard</p>
+            <hr className="my-2" />
+
+            <div>
+
+              <Form>
+                <FormGroup>
+                  <Label for="book">Book Name</Label>
+                  <Input
+                    type="text"
+                    name="book"
+                    value={bookState.book}
+                    onChange={bookState.handleInputChange}
+                  />
+                </FormGroup>
+                <Button onClick={bookState.handleBookSearch}>Search</Button>
+              </Form>
+
+            </div>
+
+
+
+
+          </Jumbotron>
+        </div>
+      </Container>
+    </>
   )
 }
 
